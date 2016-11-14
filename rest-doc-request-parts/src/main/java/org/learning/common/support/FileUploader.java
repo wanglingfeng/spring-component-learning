527ea98411f3a0ddded2094292e86df4a9f5e3d3
wanglingfeng <joseph.wlf@qq.com>
2016-11-14 17:57:07 +0800

    each project move out

--- rest-doc-request-parts/src/main/java/org/learning/common/support/FileUploader.java
+++
@@ -1,9 +0,0 @@
-package org.learning.common.support;
-
-/**
- * Created by Lingfeng on 2016/5/12.
- */
-public interface FileUploader {
-
-    String uploadFile(String fileName, byte[] data);
-}
--- test-sms-email/src/main/java/com/sms/test/service/SmsSender.java
+++
@@ -1,9 +0,0 @@
-package com.sms.test.service;
-
-/**
- * Created by Lingfeng on 2016/4/15.
- */
-public interface SmsSender {
-
-    String send(String mobile);
-}
